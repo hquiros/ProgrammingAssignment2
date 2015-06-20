b4f768ae4958f66d729ef1fa63cf5069d34e660c
hquiros <heraclio.quiros@gmail.com>
2015-06-20 12:09:55 -0500

    instructions included

--- cachematrix.R
+++ cachematrix.R
@@ -3,6 +3,15 @@
 ## did not change), the result is retrieved from cache.
 ## If not, the inverse of the matrix is computed.
 
+## Instructions:
+## 1. Assign makeCacheMatrix(your_own_matrix) to a varible.
+##    "your_own_matrix" should be a matrix object.
+## 2. If you want to check the matrix to be inversed, use: variable$get()
+## 3. If you want to check the inversed matrix, use: variable$getinverse()
+## 4. If you want to compute the inverse of a matrix (or get it from cache), use: cacheSolve(variable)
+## 5. If you want to set a matrix to be inversed, use: variable$set(your_own_matrix)
+## 6. If you want to set the inversed matrix, use: variable$setinverse(your_inversed_matrix)
+
 ## makeCacheMatrix create a list with the following functions:
 ## set: set the matrix to be inverted.
 ## get: get the matrix to be inverted.
